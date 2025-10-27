bb467e6d4430d599928ba19832eff167e230406e
Hafsah Muazzam <HM1150@live.mdx.ac.uk>
2025-10-27 23:44:57 +0400

    first commit

---
+++ activities.js
@@ -0,0 +1,47 @@
+let activities = [
+  {
+    "id": 1001,
+    "title": "Chemistry",
+    "description": "be the chemist you always wished to be",
+    "price": 80.00,
+    "image": "chemistry.jpg",
+    "availableInventory": 10,
+    "Location": "Dubai"
+  },
+  {
+    "id": 1002,
+    "title": "Physics",
+    "description": "start sloving like einstein with our extra class",
+    "price": 80.00,
+    "image": "physics.jpg",
+    "availableInventory": 10,
+    "Location": "Dubai"
+  },
+  {
+    "id": 1003,
+    "title": "Biology",
+    "description": "learn all anout human anatomy",
+    "price": 90.00,
+    "image": "biology.jpg",
+    "availableInventory": 10,
+    "Location": "Dubai"
+  },
+  {
+    "id": 1004,
+    "title": "Sports",
+    "description": "make your athlectic dreams come true",
+    "price": 100.00,
+    "image": "sports.jpg",
+    "availableInventory": 10,
+    "Location": "Dubai"
+  },
+  {
+    "id": 1005,
+    "title": "Maths",
+    "description": "training to be a human calculator!",
+    "price": 70.00,
+    "image": "biology.jpg",
+    "availableInventory": 10,
+    "Location": "Dubai"
+  }
+];
